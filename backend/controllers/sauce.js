f5e6651a94370d948f303dc806a498655dc203b9
Laurent <cuvelier05@hotmail.com>
2021-06-08 20:48:53 +0200

    modification route post avec config multer

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,9 +1,23 @@
 const Sauce = require("../models/sauce");
 
+// exports.createSauce = (req, res, next) => {
+//   delete req.body._id;
+//   const sauce = new Sauce({
+//     ...req.body,
+//   });
+//   sauce
+//     .save()
+//     .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+//     .catch((error) => res.status(400).json({ error }));
+// };
 exports.createSauce = (req, res, next) => {
-  delete req.body._id;
+  const SauceObject = JSON.parse(req.body.thing);
+  delete SauceObject._id;
   const sauce = new Sauce({
-    ...req.body,
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
   });
   sauce
     .save()

072bd9eff6d35b0d6d3f626bd09766d89d0b3c90
Laurent <cuvelier05@hotmail.com>
2021-06-08 16:33:52 +0200

    création middleware post + install body parser

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,45 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("Requête reçue !");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+app.use(bodyParser.json());
+
+app.post("/api/sauces", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
 });
 
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
+app.use("/api/sauces", (req, res, next) => {
+  const sauce = [
+    {
+      _id: "oeihfzeoi",
+      userId: "oeihfzeoi",
+      name: "Ma premiere sauce",
+      manufacturer: "Le fabricant",
+      description: "Les infos de ma premiere sauce",
+      mainPepper: "piment",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      heat: 10,
+    },
+  ];
+  res.status(200).json(sauce);
 });
 
 module.exports = app;

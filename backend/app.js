56fa2c0d5f53e6c6f71aa4a0c2779abf6ac8ae99
Laurent <cuvelier05@hotmail.com>
2021-07-02 14:50:47 +0200

    installation et utilisation de express mongo sanitize

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 require("dotenv").config();
 const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user");
+const app = express();
 
 mongoose
   .connect("mongodb+srv://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + process.env.DB_CLUSTER, {
@@ -14,10 +16,6 @@ mongoose
   })
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-
-const app = express();
-app.use(helmet());
-// app.disable("x-powered-by");
 
 // CORS (Cross Origin Ressource Sharing) => sécurité permettant contrôle des ressources partagés
 app.use((req, res, next) => {
@@ -41,5 +39,6 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/sauces", sauceRoutes);
 //utilisation routage pour les utilisateurs
 app.use("/api/auth", userRoutes);
-
+app.use(helmet());
+app.use(mongoSanitize());
 module.exports = app;
